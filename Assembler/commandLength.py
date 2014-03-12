1658cc7625c0e3f2d6a67bc14385ed5f01801cee
helchon <lauris110@gmail.com>
2014-03-12 19:22:52 +0200

    Komandų ilgis, skaičiuoja DW, DM, DS užimamus

--- Assembler/commandLength.py
+++ Assembler/commandLength.py
@@ -1,10 +1,17 @@
 def commandLength(codeLine):
     if codeLine[0].upper() in ('ADD', 'SUB', 'MUL', 'DIV', 'MOD', 'CMP', 'AND', 'OR'):
-        if codeLine[3].isdigit():
+        if codeLine[2].isdigit():
             length = 2
         else:
             length = 1
+    elif codeLine[0].upper() == 'DW':
+        length =1
+    elif codeLine[0].upper() == 'DM':
+        length = codeLine[2]
+    elif codeLine[0].upper() == 'DS':
+        length = len(codeLine[2]) / 8
+        if (len(codeLine[2]) % 8) !=0:
+            length = length +1
     else:
         length = 1
     return length
-        
