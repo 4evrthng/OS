d3b2b1e3d773dadacd96034ae86924e7c7f8d1ba
theromanosipov <theromanosipov@gmail.com>
2014-03-22 17:40:51 +0200

    Assembleris: Baveik visos komandos
    Nera NOT ir darbo su shared atmintimi

--- Assembler/assembler.py
+++ Assembler/assembler.py
@@ -1,4 +1,5 @@
 import commandLength
+import struct
 
 class Command:
 	commandCode = 0
@@ -18,9 +19,6 @@ class Label:
 	address = 0
 
 commands = {
-	'DW'			: 0 ,
-	'DB'			: 0 ,
-	'DS'			: 0 ,
 	'ADD'			: 1 ,
 	'SUB'         	: 2 ,
 	'MUL'         	: 3 ,
@@ -57,7 +55,7 @@ registers = {
 	'CX' : 3
 }
 	
-codeText = open ('C:/Users/Helch/Documents/GitHub/OS/Assembler/testProg01.txt', 'r')
+codeText = open ('C:/Users/Roman/Documents/GitHub/OS/Assembler/testProg01.txt', 'r')
 codeLineList = codeText.readlines()
 
 while '\n' in codeLineList: codeLineList.remove('\n')
@@ -82,6 +80,8 @@ for codeLine in codeLineList:
 		newVariable.address = position
 		variables.append(newVariable)
 		position += commandLength.commandLength(codeLine)
+	else:
+		position += commandLength.commandLength(codeLine)
 
 # Second pass determines the machine code and appends it to the assembledCode
 assembledCode = bytearray()
@@ -90,7 +90,6 @@ for codeLine in codeLineList:
 	#Commands that may take 1 or 2 words
 	if codeLine[0].upper() in ('ADD', 'SUB', 'MUL', 'DIV', 'MOD', 'CMP', 'AND', 'OR'):
 		assembledCode.append( commands[codeLine[0].upper()] )
-		variableAddress = -1
 		#Second operand is a register
 		if codeLine[2].upper() in ('AX', 'BX', 'CX'):
 			assembledCode.append(0x01)
@@ -106,7 +105,8 @@ for codeLine in codeLineList:
 			#Append the code of the second operand
 			assembledCode.append(0x00)
 		else:
-			#Second operand is a variable
+			#Second operand in memory
+			variableAddress = -1
 			for variable in variables:
 				if codeLine[2] == variable.name:
 					variableAddress = variable.address
@@ -117,12 +117,112 @@ for codeLine in codeLineList:
 					assembledCode.append( variableAddress)
 					break
 			#if variableAddress == -1:
-				#UNRECOGNISED COMMAND
+				#ERROR	
+		assembledCode.extend((0x00, 0x00, 0x00, 0x00))	
+		#Append a number operand
+		if codeLine[2].isdigit():
+			hexString =  struct.pack('>Q', long(codeLine[2]))
+			#if len(hexString) > 8:
+				#ERROR
+			for i in range(0, 8 - len(hexString)):
+				assembledCode.append(0x00)
+			for i in range(0, len(hexString)):
+				assembledCode.append( hexString[i])
+	
+	#Commands with no operands
+	if codeLine[0].upper() in ('FSEEK', 'FLOSE', 'FDELETE', 'EXIT'):
+		assembledCode.append( commands[codeLine[0].upper()] )
+		assembledCode.extend((0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00))
 		
-		assembledCode.extend((0x00, 0x00))
+	#Commands with one operand in the memory
+	if codeLine[0].upper() in ('IN', 'OUT', 'FOPEN', 'FREAD', 'FWRITE'):
+		assembledCode.append( commands[codeLine[0].upper()] )
+		assembledCode.append(0x00)
+		#Find the address of the variable
+		variableAddress = -1
+		for variable in variables:
+			if codeLine[1] == variable.name:
+				variableAddress = variable.address
+				#Append the address of the variable
+				assembledCode.append( variableAddress)
+				break
+		#if variableAddress == -1:
+			#ERROR codeLine[1] is not a variable
+		assembledCode.extend((0x00, 0x00, 0x00, 0x00, 0x00))
 		
+	#Commands with one label operand
+	if codeLine[0].upper() in ('JMP', 'JE', 'JNE', 'JL', 'JS', 'LOOP'):
+		assembledCode.append( commands[codeLine[0].upper()] )
+		assembledCode.append(0x00)
+		#Find the address of the label
+		labelAddress = -1
+		for label in labels:
+			if codeLine[1] == label.name:
+				labelAddress = label.address
+				#Append the address of the label
+				assembledCode.append( labelAddress)
+				break
+		#if labelAddress == -1:
+			#ERROR codeLine[1] is not a label
+		assembledCode.extend((0x00, 0x00, 0x00, 0x00, 0x00))
+			
+	#Command LOAD op1 register op2 memory
+	if codeLine[0].upper() in ('LOAD'):
+		assembledCode.append( commands[codeLine[0].upper()] )
+		assembledCode.append(0x00)
+		#if codeLine[1].upper() not in ('AX', 'BX', 'CX'):
+			#ERROR first operand must be a register
+		assembledCode.append( registers[ codeLine[1].upper()])
+		#Find the address of the variable
+		variableAddress = -1
+		for variable in variables:
+			if codeLine[2] == variable.name:
+				variableAddress = variable.address
+				#Append the address of the variable
+				assembledCode.append( variableAddress)
+				break
+		#if variableAddress == -1:
+			#ERROR codeLine[1] is not a variable
+		assembledCode.extend((0x00, 0x00, 0x00, 0x00))
 		
+	#Command STORE op1 memory op2 register
+	if codeLine[0].upper() in ('STORE'):
+		assembledCode.append( commands[codeLine[0].upper()] )
+		assembledCode.append(0x00)
+		variableAddress = -1
+		#Find the address of the variable
+		for variable in variables:
+			if codeLine[1] == variable.name:
+				variableAddress = variable.address
+				#Append the address of the variable
+				assembledCode.append( variableAddress)
+				break
+		#if variableAddress == -1:
+			#ERROR codeLine[1] is not a variable
+		#if codeLine[1].upper() not in ('AX', 'BX', 'CX'):
+			#ERROR first operand must be a register
+		assembledCode.append( registers[ codeLine[2].upper()])
+		assembledCode.extend((0x00, 0x00, 0x00, 0x00))
+	
+	#DW
+	if codeLine[0].upper() in ('DW'):
+		if len(codeLine) == 2:
+			assembledCode.extend((0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00))
+		elif codeLine[2].isdigit():
+			hexString =  struct.pack('>Q', long(codeLine[2]))
+			#if len(hexString) > 8:
+				#ERROR
+			for i in range(0, 8 - len(hexString)):
+				assembledCode.append(0x00)
+			for i in range(0, len(hexString)):
+				assembledCode.append( hexString[i])
+		#else:
+			#ERROR
 		
+	
+	
+	
+	
 codeBytes = open('codeBytes', 'wb')
 codeBytes.write(assembledCode)
 codeBytes.close()
