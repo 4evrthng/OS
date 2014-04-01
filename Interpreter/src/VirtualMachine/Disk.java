05d0842cf1badf094096216f6127fcb126e56fe6
theromanosipov <theromanosipov@gmail.com>
2014-04-01 03:46:28 +0300

    Major Disk update

--- Interpreter/src/VirtualMachine/Disk.java
+++ Interpreter/src/VirtualMachine/Disk.java
@@ -7,6 +7,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.Arrays;
 
 public class Disk {
 	
@@ -32,25 +33,167 @@ public class Disk {
 		} catch (IOException x) {
 		    System.out.println("I/O Exception: " + x);
 		}
-	}
-	
-	public void fileWrite (ByteBuffer out) {
+	}	
+	private byte getFirstSector(long fileHandle) {
 		try {
-			while (out.hasRemaining())
-				diskChanel.write(out);
+			ByteBuffer firstSector = ByteBuffer.allocate(1); 
+			diskChanel.position(fileHandle + 13);
+			diskChanel.read(firstSector);
+			return firstSector.get(0);
+				
+		} catch (IOException x) {
+			System.out.println("I/O Exception while creating a file: " + x);
+		}
+		return (byte) 0;
+	}
+	private byte getNextSector (byte currentSector) {
+		try {
+			ByteBuffer nextSector = ByteBuffer.allocate(1);
+			diskChanel.position(currentSector);
+			diskChanel.read(nextSector);
+			return nextSector.get(0);
+				
+		} catch (IOException x) {
+			System.out.println("I/O Exception: " + x);
+		}
+		return (byte) 0;
+	}
+	private void setSector (byte sector, byte valueToSet){
+		try {
+			ByteBuffer valueToSetBuff = ByteBuffer.allocate(1);
+			valueToSetBuff.put(valueToSet);
+			valueToSetBuff.rewind();
+			
+			
+			System.out.print(sector);
+			
+			diskChanel.position(sector & 0xFF);
+			diskChanel.write(valueToSetBuff);
+			
+		} catch (IOException x) {
+			System.out.println("I/O Exception: " + x);
+		}
+	}
+	private byte returnEmptySector() {
+		try {
+			for (int i = 1; i < 255; i++) {
+				diskChanel.position(i);
+				ByteBuffer sector = ByteBuffer.allocate(1);
+				diskChanel.read(sector);
+				if (sector.get(0) == (byte) 0) {
+					sector = ByteBuffer.allocate(1);
+					sector.put((byte) 255);
+					sector.rewind();
+					diskChanel.position(i);
+					diskChanel.write(sector);
+					return (byte) i;
+				}
+			}
+				
+		} catch (IOException x) {
+			System.out.println("I/O Exception: " + x);
+		}
+		return (byte) 0;
+	}
+	public long fileOpen(String fileName) {
+		try {
+			//Read the root directory which starts in sector 1 (byte 512)
+			for (long i = 512; i < 1024; i = i + 16) {
+				ByteBuffer fileNameCandidate = ByteBuffer.allocate(fileName.length());
+				diskChanel.position(i);
+				diskChanel.read(fileNameCandidate);
+				if (fileName.equals(new String(fileNameCandidate.array()))) {
+					return i;
+				}		
+			}
+			
+		}catch (IOException x) {
+		    System.out.println("I/O Exception while creating a file: " + x);
+		}
+		return 0;
+	}
+	public boolean fileCreate (String name) {
+		name = name.substring(0, Math.min(name.length(), 13));
+		try {
+			//Read the root directory which starts in sector 1 (byte 512)
+			for (int i = 512; i < 1024; i = i + 16) {
+				diskChanel.position(i);
+				ByteBuffer fileName = ByteBuffer.allocate(13);
+				diskChanel.read(fileName);
+				
+				fileName.rewind();
+				
+				//Check if the spot is empty if it is create a file there
+				if (0 == fileName.compareTo(ByteBuffer.allocate(13))) {
+					diskChanel.position(i);
+					ByteBuffer newFile = ByteBuffer.allocate(16);
+					newFile.put(ByteBuffer.wrap(name.getBytes()));
+					newFile.position(13);
+					byte firstSector = returnEmptySector();
+					newFile.put(firstSector);
+					newFile.putShort((short)0);
+					newFile.rewind();
+					
+					diskChanel.position(i);
+					diskChanel.write(newFile);
+					
+					return true;
+				}		
+			}
+			
+		}catch (IOException x) {
+		    System.out.println("I/O Exception while creating a file: " + x);
+		}
+		return false;
+	}
+	public void fileWrite (long fileHandle, byte[] data) {
+		try {
+			byte currentSector = getFirstSector(fileHandle);
+			
+			for (int i = 0; i < data.length; i += 512) {
+				
+				byte dataSector[] = Arrays.copyOfRange(data, i, Math.min(i + 512 - 1, data.length));
+				
+				diskChanel.position((currentSector & 0xFF) * 512);
+				diskChanel.write(ByteBuffer.wrap(dataSector));
+				
+				if (getNextSector(currentSector) == (byte) 255 && i + 512 < data.length){
+					setSector(currentSector, returnEmptySector());
+				}
+				
+				currentSector = getNextSector(currentSector);
+
+			}
+			
+			
 		} catch (IOException x) {
 			System.out.println("I/O Exception in fileWrite: " + x);
 		}
 	}
-	
-	public static void main(String[] args) {
+	public ByteBuffer fileRead (long fileHandle, long lenght) {
+		
+		byte firstSector = getFirstSector(fileHandle);
+		ByteBuffer output = ByteBuffer.allocate((int)lenght);
+		
+		try {
+			diskChanel.position((firstSector & 0xFF) * 512);
+			diskChanel.read(output);
+			
+		} catch (IOException x) {
+			System.out.println("I/O Exception in fileRead: " + x);
+		}
+		
+		return output;
+	}
+	public static void main(String[] args) throws IOException {
 		Disk testDisk = new Disk (new Reg8B(), new Reg8B(), new Reg8B(), new long[RM.MAX_PAGES][16], new SMem());
 		
-		String s = "I was here!\n";
-		byte data[] = s.getBytes();
-		ByteBuffer out = ByteBuffer.wrap(data);
+		//ByteBuffer temp = testDisk.fileRead(560, 100);
+		//System.out.print(new String (temp.array()));
 		
-		testDisk.fileWrite(out);
+		testDisk.fileWrite(512, "asd".getBytes());
+		
+		//System.out.print(testDisk.fileOpen("nastia"));
 	        
 	}
 }
