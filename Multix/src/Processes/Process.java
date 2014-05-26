ec0b1fbfad5d0445c293b503d8f5af42377e1fc2
4evrthng <4evrthng@gmail.com>
2014-05-26 22:22:14 +0300

    Process su cpu kintamaisias

--- Multix/src/Processes/Process.java
+++ Multix/src/Processes/Process.java
@@ -2,10 +2,16 @@ package Processes;
 
 import Devices.CPU;
 import Interfaces.StepByStep;
+import Registers.Reg8B;
+import Registers.RegB;
+import Registers.StatusFlag;
 
 public abstract class Process implements StepByStep {
-	private int ip;
+	
 	private final int maxNumberOfBlockingStuff = 1;
+	private Reg8B CS, DS, AX, BX, CX, IP;
+	private StatusFlag SF;
+	private RegB CH1, CH2, CH3, TI, SI, PI, MODE, PTR;
 	private CPU cpu = new CPU();
 	
 	public abstract void step();
