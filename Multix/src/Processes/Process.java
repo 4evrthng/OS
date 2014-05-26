d10ad2bf96e35cca5d3de7fae4025a1faec0871a
4evrthng <4evrthng@gmail.com>
2014-05-26 22:06:15 +0300

    initial process abstract

--- Multix/src/Processes/Process.java
+++ Multix/src/Processes/Process.java
@@ -1,9 +1,34 @@
 package Processes;
 
+import Devices.CPU;
 import Interfaces.StepByStep;
 
 public abstract class Process implements StepByStep {
-
-	public abstract void step();
+	private int ip;
+	private final int maxNumberOfBlockingStuff = 1;
+	private CPU cpu = new CPU();
+	
+	public void step()
+	{
+		if (ip != maxNumberOfBlockingStuff)
+		{
+			ip ++;
+		}
+		this.chooseStep(ip);
+		//go back to cpu
+	}
+	private void chooseStep(int ip)
+	{
+		switch (ip)
+		{
+		case 1:  
+			//do stuff with cpu?
+			break;
+		default:
+			break;
+		}
+		
+	}
+	
 
 }
